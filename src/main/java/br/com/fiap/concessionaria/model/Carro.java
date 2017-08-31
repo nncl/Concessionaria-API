343b8ba5c3f1287a6530d22e9256c57f204586c5
caue <nncl@live.com>
2017-08-30 21:15:33 -0300

    Adding models

---
+++ src/main/java/br/com/fiap/concessionaria/model/Carro.java
@@ -0,0 +1,40 @@
+package br.com.fiap.concessionaria.model;
+
+public class Carro {
+    private int id;
+    private String modelo;
+    private int ano;
+    private Fabricante fabricante;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+}
